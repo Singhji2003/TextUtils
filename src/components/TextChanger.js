324daf4e1a1f5747546cdfb36023fa0340723018
Arpan <arpansinghrajput123@gmail.com>
2022-09-28 21:38:36 +0530

    Initial Commit

--- src/components/TextChanger.js
+++ src/components/TextChanger.js
@@ -34,16 +34,16 @@ export default function TextChanger(props) {
         <h2 className={`text-${props.mode==='light' ? "black" :'light'}`}>Enter the text here</h2>
       <div className="mb-3">
   <textarea className={`form-control text-${props.mode==='light' ? 'black' :'light'}` } value={text} style = {{backgroundColor: props.mode==='light' ? "white" :'#212529' }} onChange={handler} id="exampleFormControlTextarea1" rows="5"></textarea>
-  <button type="button" onClick={upperCase} className="btn btn-info my-2">Convert to Upper Case</button>
+  <button  type="button" onClick={upperCase} className="btn btn-info my-2">Convert to Upper Case</button>
   <button type="button" onClick={lowerCase} className="btn btn-info mx-3 my-2">Convert to Lower Case</button>
   <button type="button" onClick={copyText} className="btn btn-info mx-3 my-2">{copy}</button>
  
 </div>
     <h2 className={`text-${props.mode==='light' ? "black" :'light'}`} >Details about words</h2>
-    <p className = {`text-${props.mode==='light' ? 'black' :'light'}`}>Words Count: {text.split(" ").length-1} </p>
-    <p className = {`text-${props.mode==='light' ? 'black' :'light'}`}>CharactersCount:{text.length} </p>
+    <p className = {`text-${props.mode==='light' ? 'black' :'light'}`}>Words Count: {text.split(" ").filter((element)=>{return element.length!==0}).length} </p>
+    <p className = {`text-${props.mode==='light' ? 'black' :'light'}`}>Characters Count:{text.length} </p>
     <h2 className = {`text-${props.mode==='light' ? 'black' :'light'}`}>Preview</h2>
-    <p className = {`text-${props.mode==='light' ? 'black' :'light'}`}>{text}</p>
+    <p className = {`text-${props.mode==='light' ? 'black' :'light'}`}>{text.length>0? text: "Enter something to preview"}</p>
     </div> 
   )
 }
