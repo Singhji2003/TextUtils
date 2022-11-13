60be844ff4957c85e7af198ff5dcde5b2458d250
= <=>
2022-11-13 10:07:19 +0530

    ok

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,8 @@ export default function Navbar(props) {
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-  <a class="navbar-brand" href="#">
-      <img src="https://imgs.search.brave.com/bIILt83rOwJdLzzwJgSBpKyKpO_C8Ae8oLAvZ6_-ctM/rs:fit:100:100:1/g:ce/aHR0cHM6Ly9kb3du/bG9hZC5jbmV0LmNv/bS9hL2ltZy9jYXRh/bG9nLzIwMTcvMTEv/MTQvZDg5NWUxYWMt/ZGIwMi00NDBmLWE2/NjItMzBiNDE0YzQy/NTRkL2ljb25pbWc2/NTA3Ni5wbmc" alt="Logo" width="30" height="30" class="d-inline-block align-text-top mx-2"/>
+  <a className="navbar-brand" href="/">
+      <img src="https://imgs.search.brave.com/bIILt83rOwJdLzzwJgSBpKyKpO_C8Ae8oLAvZ6_-ctM/rs:fit:100:100:1/g:ce/aHR0cHM6Ly9kb3du/bG9hZC5jbmV0LmNv/bS9hL2ltZy9jYXRh/bG9nLzIwMTcvMTEv/MTQvZDg5NWUxYWMt/ZGIwMi00NDBmLWE2/NjItMzBiNDE0YzQy/NTRkL2ljb25pbWc2/NTA3Ni5wbmc" alt="Logo" width="30" height="30" className="d-inline-block align-text-top mx-2"/>
       {props.title}
     </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './style.css'
 export default function Textarea() {
+  const {text, setText} = useState("Enter the text here")
   return (
     <div>
-      <div class="mb-3">
-  <label for="text" class="form-label my-3">Enter your text to Analyze</label>
-  <textarea class="form-control" id="text" rows="3"></textarea>
+      <div className="mb-3">
+  <label htmlFor="text" className="form-label my-3">Enter your text to Analyze</label>
+  <textarea className="form-control" value={text} id="text" rows="7"></textarea>
+  <div className="left">
   <button id='upper'>Convert to UpperCase</button>
   <button id='lower'>Convert to LowerCase</button>
-  <button id='copy'>Copy the Text</button>
   <button id='remove'>Remove Extra Spaces</button>
-  <button id='rem'>Remove all Word</button>
+  </div>
+  <div className="right">
+  <button id='pswd'>Password Generator</button>
+  <button id='copy'>Copy the Text</button>
+  <button id='rem'>Clear Text</button>
+  </div>
 </div>
     </div>
   )
